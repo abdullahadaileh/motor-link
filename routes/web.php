b2aa8a893980dc1f3e2f14f80a2718a0a6da3189
abdullahadaileh <abdullahadaileh957@gmail.com>
2024-10-14 23:20:55 +0300

    update chart page

--- routes/web.php
+++ routes/web.php
@@ -58,9 +58,9 @@ Route::get('/motor-link-vehicles/{vehicle}', [VehicleController::class, 'showLan
 
 // Dashboard Page Routs
 
-// Route::get('/motor-link-dashboard', function () {
-//     return view('dashboard.dashboard');
-// })->name('motor-link-dashboard');
+Route::get('/motor-link-dashboard', function () {
+    return view('dashboard.dashboard');
+})->name('motor-link-dashboard');
 
 Route::get('/motor-link-dashboard', [DashboardController::class, 'dashboard'])->name('motor-link-dashboard');
 
