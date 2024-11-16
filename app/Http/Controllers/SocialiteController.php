d908ec0b7f4bdbd75474868389f72d343ec49fef
abdullahadaileh <abdullahadaileh957@gmail.com>
2024-11-16 17:40:45 +0300

    update

--- app/Http/Controllers/SocialiteController.php
+++ app/Http/Controllers/SocialiteController.php
@@ -14,25 +14,21 @@ class SocialiteController extends Controller
         return Socialite::driver('google')->redirect();
     }
 
-    public function redirectToFacebook()
-    {
-        return Socialite::driver('facebook')->redirect();
-    }
 
     public function handleGoogleCallback()
     {
         $user = Socialite::driver('google')->user();
 
         try {
-            // البحث عن المستخدم بواسطة البريد الإلكتروني
+
             $finduser = User::where('email', $user->getEmail())->first();
 
             if ($finduser) {
-                // إذا كان المستخدم موجودًا ويمتلك social_id، قم بتسجيل الدخول
+
                 if ($finduser->social_id) {
                     Auth::login($finduser);
                 } else {
-                    // إذا كان المستخدم موجودًا ولكن بدون social_id، قم بتحديث بياناته
+
                     $finduser->update([
                         'social_id' => $user->id,
                         'social_type' => 'google',
@@ -41,7 +37,7 @@ class SocialiteController extends Controller
                 }
                 return redirect('/motor-link');
             } else {
-                // إذا كان المستخدم غير موجود، قم بإنشاء حساب جديد
+
                 $newUser = User::create([
                     'name' => $user->getName(),
                     'email' => $user->getEmail(),
@@ -58,42 +54,4 @@ class SocialiteController extends Controller
         }
     }
 
-    public function handleFacebookCallback()
-    {
-        $user = Socialite::driver('facebook')->user();
-
-        try {
-            // البحث عن المستخدم بواسطة البريد الإلكتروني
-            $finduser = User::where('email', $user->getEmail())->first();
-
-            if ($finduser) {
-                // إذا كان المستخدم موجودًا ويمتلك social_id، قم بتسجيل الدخول
-                if ($finduser->social_id) {
-                    Auth::login($finduser);
-                } else {
-                    // إذا كان المستخدم موجودًا ولكن بدون social_id، قم بتحديث بياناته
-                    $finduser->update([
-                        'social_id' => $user->id,
-                        'social_type' => 'facebook',
-                    ]);
-                    Auth::login($finduser);
-                }
-                return redirect('/motor-link');
-            } else {
-                // إذا كان المستخدم غير موجود، قم بإنشاء حساب جديد
-                $newUser = User::create([
-                    'name' => $user->getName(),
-                    'email' => $user->getEmail(),
-                    'social_id' => $user->id,
-                    'social_type' => 'facebook',
-                    'password' => encrypt('my-facebook'),
-                ]);
-
-                Auth::login($newUser);
-                return redirect('/motor-link');
-            }
-        } catch (\Throwable $th) {
-            dd('error' . $th->getMessage());
-        }
-    }
 }
